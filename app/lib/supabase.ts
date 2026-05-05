0da1e50711813f19b2b026d59e9a31d220a650dd
lokhandemedhir-droid <lokhandemedhir@gmail.com>
2026-05-05 20:36:52 +0530

    fix supabase client

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://pekgrvqgrlumtvvzecmy.supabase.co"
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBla2dydnFncmx1bXR2dnplY215Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDU1NzIsImV4cCI6MjA5MzQ4MTU3Mn0.Bfa7FhkLQtx173js4iE9bveHPjxEDSrW4f69AQV8Olw"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createSupabaseClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
